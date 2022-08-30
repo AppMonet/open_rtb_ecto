a64230b16c86a4f5e5949995f52fc75ba3f544ad
Louis Cameron Booth <me@louiscb.com>
2022-08-30 11:17:33 +0200

    Update helper class

--- lib/open_rtb_ecto/v2/native/helper.ex
+++ lib/open_rtb_ecto/v2/native/helper.ex
@@ -1,23 +1,27 @@
 defmodule OpenRtbEcto.V2.Native.Helper do
   import Ecto.Changeset
 
-  # Each object may contain only one of title, img, data or video.
-  def validate_media(asset) do
+  @doc """
+  Validates that there is max one item from the media list present in the changeset of the response and request
+  asset objects.
+  """
+  @spec validate_media(Ecto.Changeset.t) :: Ecto.Changeset.t
+  def validate_media(changeset) do
     media = [:title, :img, :data, :video]
 
     total =
       media
-      |> Enum.map(fn field -> if get_field(asset, field), do: 1, else: 0 end)
+      |> Enum.map(fn field -> if get_field(changeset, field), do: 1, else: 0 end)
       |> Enum.sum()
 
     if total > 1 do
       add_error(
-        asset,
+        changeset,
         :media,
-        "asset object may contain only one of title, img, data or video"
+        "changeset object may contain only one of title, img, data or video"
       )
     else
-      asset
+      changeset
     end
   end
 end
