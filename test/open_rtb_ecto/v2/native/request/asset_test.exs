bf0b27a90a343038a5f567bd339bacd651aefdd4
Louis Cameron Booth <me@louiscb.com>
2022-08-30 11:18:31 +0200

    Update tests

--- test/open_rtb_ecto/v2/native/request/asset_test.exs
+++ test/open_rtb_ecto/v2/native/request/asset_test.exs
@@ -8,13 +8,13 @@ defmodule OpenRtbEcto.V2.Native.Request.AssetTest do
     test "cast will return error as data contains title and video" do
       data = TestHelper.test_data("v2/request", "incorrect-assets-data-title-video.json")
       assert {:error, %{media: error}} = OpenRtbEcto.cast(Asset, data)
-      assert ["asset object may contain only one of title, img, data or video, got nil"] = error
+      assert ["changeset object may contain only one of title, img, data or video, got nil"] = error
     end
 
     test "cast will return error as data contains all media" do
       data = TestHelper.test_data("v2/request", "incorrect-assets-data-all-media.json")
       assert {:error, %{media: error}} = OpenRtbEcto.cast(Asset, data)
-      assert ["asset object may contain only one of title, img, data or video, got nil"] = error
+      assert ["changeset object may contain only one of title, img, data or video, got nil"] = error
     end
   end
 
