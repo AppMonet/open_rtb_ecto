35a929bbf20b0b405eab449ae29e4d80b4fca8a1
Nick Jacob <nickbjacob@gmail.com>
2023-09-13 09:10:21 -0400

    fix flakey test, add video.plcmt

--- test/open_rtb_ecto/v2/bid_request_test.exs
+++ test/open_rtb_ecto/v2/bid_request_test.exs
@@ -163,7 +163,11 @@ defmodule OpenRtbEcto.V2.BidRequestTest do
 
   describe "json encoding" do
     test "fields with nil values are omitted" do
-      assert "{\"at\":2,\"test\":0,\"allimps\":0}" == Jason.encode!(%BidRequest{})
+      str = Jason.encode!(%BidRequest{})
+      map = Jason.decode!(str)
+
+      assert 3 == map_size(map)
+      assert ["allimps", "at", "test"] = Enum.sort(Map.keys(map))
     end
   end
 end
