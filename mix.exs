37ced6682956bc7d5964600f229472ed849e08ea
nico piderman <nico.piderman@gmail.com>
2025-05-02 15:03:43 +0200

    bump ver, update deps

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule OpenRtbEcto.MixProject do
   use Mix.Project
 
   @name "OpenRtbEcto"
-  @version "1.3.0"
+  @version "1.4.0"
   @repo_url "https://github.com/AppMonet/open_rtb_ecto"
 
   def project do
