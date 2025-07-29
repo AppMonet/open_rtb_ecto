e9767272cccb02ce4f5239deba96090b1a1167ff
nico piderman <nico.piderman@gmail.com>
2025-07-29 11:24:58 +0200

    add consent field to user object

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule OpenRtbEcto.MixProject do
   use Mix.Project
 
   @name "OpenRtbEcto"
-  @version "1.4.2"
+  @version "1.4.3"
   @repo_url "https://github.com/AppMonet/open_rtb_ecto"
 
   def project do
